9536f99d7bdcee9301dd100ada67fe951caff18c
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-03-05 16:18:14 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install git 
-git clone https://github.com/Sada-Siva-Reddy07/fish.git
-cd fish
+git clone https://github.com/Sada-Siva-Reddy07/penguin.git
+cd penguin
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
