b9e84c81f0cef39d7e3d4e3221af9c60be6aa187
SADA SIVA REDDY <145244419+Sada-Siva-Reddy07@users.noreply.github.com>
2024-03-05 15:49:49 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install git 
-git clone https://github.com/Sada-Siva-Reddy07/flight-perdiction.git
+git clone https://github.com/Sada-Siva-Reddy07/fish.git
 cd flight-perdiction
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
